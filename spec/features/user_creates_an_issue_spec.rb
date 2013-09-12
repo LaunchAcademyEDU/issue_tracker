6a23a3cf9c9e6242e841c5536ca6dae807348728
Eric Kelly <heroiceric@gmail.com>
2013-09-12 17:35:36 -0400

    Selecting issue category from select

--- spec/features/user_creates_an_issue_spec.rb
+++ spec/features/user_creates_an_issue_spec.rb
@@ -10,7 +10,9 @@ feature "User creates an issue", %q{
   # * I must provide a title
   # * I must provide description
   # * I can mark the issue as reproducible
-  # * I can specify the category of the issue
+  # * I must choose a category for the issue
+  # * I can categorize an issue as either "Bug", "Feature Request", or "Customer Service Issue"
+  # * I can rate the severity of the `Issue`
 
   context "with valid attributes" do
     it "creates an issue with valid attributes" do
@@ -19,14 +21,14 @@ feature "User creates an issue", %q{
       fill_in "Title", with: "Broken link"
       fill_in "Description", with: "The sign in link is broken"
       check "Is reproducible"
-      fill_in "Category", with: "bug"
-      click_on "Create Issue" 
+      select "Feature Request"
+      click_on "Create Issue"
 
       expect(page).to have_content "Issue was successfully created"
     end
   end
 
-a context "with invalid attributes" do
+  context "with invalid attributes" do
     it "sees errors for invalid attributes" do
       visit '/issues/new'
 
--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -3,4 +3,18 @@ require 'spec_helper'
 describe Issue do
   it { should_not have_valid(:title).when(nil) }
   it { should_not have_valid(:description).when(nil) }
+
+  describe '#categories' do
+    it 'includes Bug' do
+      expect(Issue.categories).to include('Bug')
+    end
+
+    it 'includes Feature Request' do
+      expect(Issue.categories).to include('Feature Request')
+    end
+
+    it 'includes Customer Service' do
+      expect(Issue.categories).to include('Customer Service')
+    end
+  end
 end
